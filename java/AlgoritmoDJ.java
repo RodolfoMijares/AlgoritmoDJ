2a4477388dea89a99d5a82897e5180d36f5b8b74
RodolfoMijares <rodolfmijars@gmail.com>
2021-10-19 14:21:55 -0500

    add my code

--- java/AlgoritmoDJ.java
+++ java/AlgoritmoDJ.java
@@ -15,8 +15,77 @@ class AlgoritmoDJ{
      * En este metodo se incia el algoritmo
      */
     private void start(){
-        System.out.println("Iniciando configuracion Dijkstra....");
-    
+    public class Dijkstra {
+
+static final int V  = 9;
+ 
+private static int minDistance(int[] dist, boolean[] verticeYaProcesado)
+{
+	   int min = Integer.MAX_VALUE; int min_index=0;
+	 
+	   for (int v = 0; v < V; v++)
+		 if (verticeYaProcesado[v] == false && dist[v] <= min) {
+			 min = dist[v];
+			 min_index = v;
+		  }
+	 
+	   return min_index;
+	}
+
+	private static void printSolution(int[] dist, int n)
+	{
+	   System.out.println("Distancia del vertice desde el origen\n");
+	   for (int i = 0; i < V; i++)
+		  System.out.println(i + " \t\t " + dist[i]);
+	}
+	 
+	private static void dijkstra(int[][] grafo, int src)
+	{
+		 int[] dist = new int[V];     
+
+	 
+		 boolean[] verticeYaProcesado = new boolean[V]; 
+
+		 for (int i = 0; i < V; i++) {
+			dist[i] = Integer.MAX_VALUE;
+			verticeYaProcesado[i] = false;
+		 }
+		 dist[src] = 0;
+	 
+		 for (int count = 0; count < V-1; count++)
+		 {
+
+		   int u = minDistance(dist, verticeYaProcesado);
+	 
+		   verticeYaProcesado[u] = true;
+	 
+		   for (int v = 0; v < V; v++)
+
+			 if (!verticeYaProcesado[v] && grafo[u][v] > 0 && dist[u] != Integer.MAX_VALUE 
+										   && dist[u]+grafo[u][v] < dist[v])
+				dist[v] = dist[u] + grafo[u][v];
+		 }
+	 
+		 // se imprime el arreglo con las distancias
+		 printSolution(dist, V);
+	}
+
+	public static void main(String[] args)
+	{
+	   int[][] graph = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+						  {4, 0, 8, 0, 0, 0, 0, 11, 0},
+						  {0, 8, 0, 7, 0, 4, 0, 0, 2},
+						  {0, 0, 7, 0, 9, 14, 0, 0, 0},
+						  {0, 0, 0, 9, 0, 10, 0, 0, 0},
+						  {0, 0, 4, 0, 10, 0, 2, 0, 0},
+						  {0, 0, 0, 14, 0, 2, 0, 1, 6},
+						  {8, 11, 0, 0, 0, 0, 1, 0, 7},
+						  {0, 0, 2, 0, 0, 0, 6, 7, 0}
+						 };
+	 
+		dijkstra(graph, 0);
+	}
+	}
     } 
     public static void main(String[] args) {
         AlgoritmoDJ algoritmoDJ = new AlgoritmoDJ();
